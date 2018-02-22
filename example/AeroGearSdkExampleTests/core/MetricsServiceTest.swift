defd6604ed51fac36c535a95f5fe4aceb8a5f1b9
Peter Braun <pbraun@redhat.com>
2018-02-22 16:54:10 +0100

    added a simple test

---
+++ example/AeroGearSdkExampleTests/core/MetricsServiceTest.swift
@@ -0,0 +1,27 @@
+@testable import AGSCore
+import Foundation
+import XCTest
+
+class MetricsServiceTest: XCTestCase {
+    func testMetricsPresent() {
+        let metrics: AgsMetrics = AgsCore.instance.getMetricsService()
+        XCTAssertNotNil(metrics)
+    }
+
+    func testDeviceMetrics() {
+        let metrics: DeviceMetrics = DeviceMetrics();
+        XCTAssert(metrics.identifier == "device")
+        let data: MetricsData = metrics.collect()
+        XCTAssertNotNil(data["platform"])
+        XCTAssertNotNil(data["platformVersion"])
+        XCTAssertNotNil(data["device"])
+    }
+
+    func testAppMetrics() {
+        let metrics: AppMetrics = AppMetrics(AgsCore.getMetadata())
+        let data: MetricsData = metrics.collect()
+        XCTAssertNotNil(data["appId"])
+        XCTAssertNotNil(data["appVersion"])
+        XCTAssertNotNil(data["sdkVersion"])
+    }
+}
