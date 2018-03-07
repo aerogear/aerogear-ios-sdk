5770127c23318b60e80ad23cf18630f88951228b
Wojciech Trocki <wtrocki@redhat.com>
2018-03-07 10:59:57 +0000

    Remove core dependency for docs

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -9,4 +9,6 @@ echo "Generating core documentation"
 (cd modules/core && bundle exec jazzy --podspec agscore.podspec --output ../../docs/api/core)
 
 echo "Generating auth documentation"
+## Remove core dependency
+sed -i.bck "s/s.dependency 'AGSCore'/ /g" modules/auth/agsauth.podspec
 (cd modules/auth && bundle exec jazzy --podspec agsauth.podspec --output ../../docs/api/auth)
