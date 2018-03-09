4a12580f211f694d7d0b6e08dd2fd1c99877dcb9
Massimiliano Ziccardi <mziccard@redhat.com>
2018-03-09 14:37:52 +0100

    Moved OIDAuth dependency to its own method for test purpose

--- modules/auth/authenticator/OIDCAuthenticator.swift
+++ modules/auth/authenticator/OIDCAuthenticator.swift
@@ -7,12 +7,12 @@ import AGSCore
 import AppAuth
 
 public class OIDCAuthenticator: Authenticator {
-
+    
     let http: AgsHttpRequestProtocol
     let keycloakConfig: KeycloakConfig
     let authConfig: AuthenticationConfig
     let credentialManager: CredentialManagerProtocol
-
+    
     var currentAuthorisationFlow: OIDAuthorizationFlowSession?
     
     init(http: AgsHttpRequestProtocol, keycloakConfig: KeycloakConfig, authConfig: AuthenticationConfig, credentialManager: CredentialManagerProtocol) {
@@ -21,7 +21,7 @@ public class OIDCAuthenticator: Authenticator {
         self.authConfig = authConfig
         self.credentialManager = credentialManager
     }
-
+    
     public func authenticate(presentingViewController: UIViewController, onCompleted: @escaping (User?, Error?) -> Void) {
         let oidServiceConfiguration = OIDServiceConfiguration(authorizationEndpoint: self.keycloakConfig.authenticationEndpoint, tokenEndpoint: self.keycloakConfig.tokenEndpoint)
         let oidAuthRequest = OIDAuthorizationRequest(configuration: oidServiceConfiguration,
@@ -32,7 +32,7 @@ public class OIDCAuthenticator: Authenticator {
                                                      additionalParameters: nil)
         
         //this will automatically exchange the token to get the user info
-        self.currentAuthorisationFlow = OIDAuthState.authState(byPresenting: oidAuthRequest, presenting: presentingViewController) {
+        self.currentAuthorisationFlow = startAuthorizationFlow(byPresenting: oidAuthRequest, presenting: presentingViewController) {
             authState,error in
             
             if let state = authState {
@@ -45,6 +45,10 @@ public class OIDCAuthenticator: Authenticator {
                 self.authFailure(error: error, onCompleted: onCompleted)
             }
         }
+    }
+    
+    func startAuthorizationFlow(byPresenting: OIDAuthorizationRequest, presenting: UIViewController, callback: @escaping OIDAuthStateAuthorizationCallback) -> OIDAuthorizationFlowSession {
+        return OIDAuthState.authState(byPresenting: byPresenting, presenting: presenting, callback: callback)
     }
     
     func authSuccess(authState: OIDAuthState, onCompleted: @escaping (User?, Error?) -> Void) {
@@ -75,8 +79,8 @@ public class OIDCAuthenticator: Authenticator {
      Otherwise it will invoke the onCompleted callback with an error.
      
      - parameters:
-       - currentUser: the user that should be logged out
-       - onCompleted: a block function that will be invoked when the logout is completed.
+     - currentUser: the user that should be logged out
+     - onCompleted: a block function that will be invoked when the logout is completed.
      */
     public func logout(currentUser: User, onCompleted: @escaping (Error?) -> Void) {
         guard let identityToken = currentUser.identityToken else {
@@ -94,3 +98,4 @@ public class OIDCAuthenticator: Authenticator {
         })
     }
 }
+
