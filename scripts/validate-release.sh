ce4dff3e8633aad6204d4341bc9048eebc60eb63
Dara Hayes <dara.hayes@redhat.com>
2018-04-20 13:07:45 +0100

    ci: validate-release.sh checks that version numbers in podspec files match

--- scripts/validate-release.sh
+++ scripts/validate-release.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-MODULES="core auth push"
+
 SDK_VERSION=`cat VERSION|tr -d '[:space:]'`
+declare -a modules=("AGSCore" "AGSAuth" "AGSPush")
 
 # explicit declaration that this script needs a $TAG variable passed in e.g TAG=1.2.3 ./script.sh
 TAG=$TAG
@@ -17,6 +18,22 @@ if [[ $TAG != $SDK_VERSION ]]; then
   echo "tag $TAG is not the same as sdk version $SDK_VERSION"
   exit
 fi
+
+## check each podspec and ensure version numbers match
+for i in "${modules[@]}"
+do
+   specname="$i.podspec"
+   spec=`cat $specname`
+   
+   match="s.version[[:space:]]*=[[:space:]]'$SDK_VERSION'"
+   
+   if [[ $spec =~ $match ]]; then
+     echo "$specname valid"
+   else
+     echo "version number mismatch in $specname"
+     exit 1;
+   fi
+done
 
 echo "TAG and SDK_VERSION are valid"
 
