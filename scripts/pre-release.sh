ff4f7713ec99cee9a056648cd63e0760ba016aab
Dara Hayes <dara.hayes@redhat.com>
2018-04-20 12:12:31 +0100

    chore: replace the sdkversion value in AGSMetadata.swift properly

--- scripts/pre-release.sh
+++ scripts/pre-release.sh
@@ -3,8 +3,11 @@
 MODULES="core auth push"
 SDK_VERSION=`cat VERSION|tr -d '[:space:]'`
 
+META_SDK_SEARCH='(sdkVersion[[:space:]]=[[:space:]])(\")([[:digit:]].[[:digit:]].[[:digit:]])(\")'
+META_SDK_REPLACE="\1\2${SDK_VERSION}\4"
+
 echo "Update SDK Version to $SDK_VERSION"
-sed -i.bak -E "s/DEVELOPMENT/${SDK_VERSION}/" ./modules/core/data/AgsMetadata.swift
+sed -i.bak -E "s/${META_SDK_SEARCH}/${META_SDK_REPLACE}/" ./modules/core/data/AgsMetadata.swift
 sed -i.bak -E "s/\.version([ ]*)=([ ]*)(.*)/\.version\1=\2'$SDK_VERSION'/g" ./*.podspec
 
 echo "SDK version is updated to $SDK_VERSION"
