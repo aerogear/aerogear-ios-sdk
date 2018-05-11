7e4d9ed035098b65c9532dc02299cea8d575f2cd
Jan Hellar <jhellar@redhat.com>
2018-05-11 11:16:18 +0200

    Delete auth and push screens from the example app in the SDK repo

--- example/AeroGearSdkExample/AppDelegate.swift
+++ example/AeroGearSdkExample/AppDelegate.swift
@@ -1,24 +1,10 @@
-import AGSAuth
 import AGSCore
-import AGSPush
 import UIKit
 import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
-    var pushHelper = PushHelper()
-
-    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey: Any] = [:]) -> Bool {
-        do {
-            return try AgsAuth.instance.resumeAuth(url: url as URL)
-        } catch AgsAuth.Errors.serviceNotConfigured {
-            print("AeroGear auth service is not configured")
-        } catch {
-            fatalError("Unexpected error: \(error).")
-        }
-        return false
-    }
 
     func applicationWillResignActive(_: UIApplication) {
     }
@@ -33,25 +19,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillTerminate(_: UIApplication) {
-    }
-
-    func application(_: UIApplication,
-                     didFinishLaunchingWithOptions _: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        pushHelper.setupPush()
-        return true
-    }
-
-    func application(_ application: UIApplication,
-                     didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-        pushHelper.registerUPS(deviceToken)
-    }
-
-    func application(_ application: UIApplication,
-                     didFailToRegisterForRemoteNotificationsWithError error: Error) {
-        pushHelper.onRegistrationFailed(error)
-    }
-
-    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any], fetchCompletionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
-        pushHelper.onPushMessageReceived(userInfo, fetchCompletionHandler)
     }
 }
--- example/AeroGearSdkExample/RootViewController.swift
+++ example/AeroGearSdkExample/RootViewController.swift
@@ -3,10 +3,6 @@ import UIKit
 class RootViewController: BaseViewController, DrawerMenuDelegate {
     // swiftlint:disable identifier_name
     static let MENU_HOME_TITLE = "Home"
-    // swiftlint:disable identifier_name
-    static let MENU_AUTHENTICATION_TITLE = "Authentication"
-    // swiftlint:disable identifier_name
-    static let MENU_PUSH_TITLE = "Push"
 
     let mainStoryBoard = UIStoryboard(name: "Main", bundle: nil)
 
@@ -15,8 +11,6 @@ class RootViewController: BaseViewController, DrawerMenuDelegate {
         menuDelegate = self
         // Do any additional setup after loading the view.
         addMenuItem(titleOfChildView: RootViewController.MENU_HOME_TITLE, iconName: "ic_home")
-        addMenuItem(titleOfChildView: RootViewController.MENU_AUTHENTICATION_TITLE, iconName: "ic_account_circle")
-        addMenuItem(titleOfChildView: RootViewController.MENU_PUSH_TITLE, iconName: "ic_notification")
         showFirstChild()
     }
 
@@ -30,10 +24,6 @@ class RootViewController: BaseViewController, DrawerMenuDelegate {
             switch selectedMenuItem.title {
             case RootViewController.MENU_HOME_TITLE:
                 launchHomeView()
-            case RootViewController.MENU_AUTHENTICATION_TITLE:
-                launchAuthView()
-            case RootViewController.MENU_PUSH_TITLE:
-                launchPushView()
             default:
                 print("no view to show")
             }
@@ -43,15 +33,5 @@ class RootViewController: BaseViewController, DrawerMenuDelegate {
     func launchHomeView() {
         let homeViewController = HomeViewController.loadViewController()
         presentViewController(homeViewController)
-    }
-
-    func launchAuthView() {
-        let authViewController = AuthViewController.loadViewController()
-        presentViewController(authViewController)
-    }
-
-    func launchPushView() {
-        let pushViewController = PushViewController.loadViewController()
-        presentViewController(pushViewController)
     }
 }
