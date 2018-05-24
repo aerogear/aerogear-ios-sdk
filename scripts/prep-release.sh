146d7ba8f2e6f40674a135adb53bf89aa4827396
Dara Hayes <dara.hayes@redhat.com>
2018-05-24 14:03:39 +0100

    ci: fix regex used for validating version numbers (#121)

--- scripts/prep-release.sh
+++ scripts/prep-release.sh
@@ -3,7 +3,7 @@
 GIT_PUSH="${GIT_PUSH:-1}"
 SDK_VERSION=`cat VERSION|tr -d '[:space:]'`
 
-META_SDK_SEARCH='(sdkVersion[[:space:]]=[[:space:]])(\")([[:digit:]].[[:digit:]].[[:digit:]])(\")'
+META_SDK_SEARCH='(sdkVersion[[:space:]]=[[:space:]])(\")([0-9]+\.[0-9]+\.[0-9]+(-.+)*)(\")'
 META_SDK_REPLACE="\1\2${SDK_VERSION}\4"
 
 echo "Update SDK Version to $SDK_VERSION"
