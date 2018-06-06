bfe063efb764f5fc3c70c3f700e89c73569ce765
Stephen Coady <scoady@redhat.com>
2018-06-06 14:19:34 +0100

    fix script error

--- modules/core/data/AgsMetadata.swift
+++ modules/core/data/AgsMetadata.swift
@@ -6,7 +6,7 @@ import Foundation
 public struct AgsMetaData {
     /** Version of Core SDK */
     // NOTE: This version is being replacted by automation script
-    public let sdkVersion = "0.4.0
+    public let sdkVersion = "0.4.0"
 
     /** Unique client id */
     public var clientId: String!
