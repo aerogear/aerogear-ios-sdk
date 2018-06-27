b9c1848f9afbe8563e3585c3757dbb50d608821c
Wei Li <wei.li@redhat.com>
2018-06-27 13:01:01 +0100

    fix regex for updating the version number in the core metadata class

--- scripts/prep-release.sh
+++ scripts/prep-release.sh
@@ -3,8 +3,8 @@
 GIT_PUSH="${GIT_PUSH:-1}"
 SDK_VERSION=`cat VERSION|tr -d '[:space:]'`
 
-META_SDK_SEARCH='(sdkVersion[[:space:]]=[[:space:]])(\")([0-9]+\.[0-9]+\.[0-9]+(-.+)*)(\")'
-META_SDK_REPLACE="\1\2${SDK_VERSION}\4\5"
+META_SDK_SEARCH='(sdkVersion[[:space:]]=[[:space:]])\"(.*)\"'
+META_SDK_REPLACE="\1\"${SDK_VERSION}\""
 
 echo "Update SDK Version to $SDK_VERSION"
 sed -i.bak -E "s/${META_SDK_SEARCH}/${META_SDK_REPLACE}/" ./modules/core/data/AgsMetadata.swift
